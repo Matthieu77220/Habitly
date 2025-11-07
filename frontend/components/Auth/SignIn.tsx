889250b8e5c553d58c08f842d88ff26b6a622357
Matthieu_772 <matthieu.dietrich77@gmail.com>
2025-11-08 00:46:42 +0100

    :clown_face: j'avais oublié d'enregistrer SignIn avant de commit

---
+++ frontend/components/Auth/SignIn.tsx
@@ -0,0 +1,13 @@
+function SignIn() {
+    return ( 
+        <>
+            <html>
+                <body>
+                    <form action=""></form>
+                </body>
+            </html>
+        </>
+     );
+}
+
+export default SignIn;
